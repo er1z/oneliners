fb6ccb46c87bdf104eae13bd2f83456726156c9d
Przemysław eRIZ Pawliczuk <er1z@users.noreply.github.com>
2019-07-25 00:32:46 +0200

    Update esxi_upgrade.sh

--- esxi_upgrade.sh
+++ esxi_upgrade.sh
@@ -1,3 +1,9 @@
 esxcli network firewall ruleset set -e true -r httpClient
 esxcli software sources profile list -d https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml | grep ESXi-6.5.0
 esxcli software profile update -d https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml -p ESXi-6.5.0-20170702001-standard
+
+# no disk space error
+
+wget http://hostupdate.vmware.com/software/VUM/PRODUCTION/main/esx/vmw/vib20/tools-light/<package name>.vib
+esxcli software vib install -f -v /tmp/<package name>.vib
+esxcli software profile update -d https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml -p ESXi-6.5.0-20170702001-standard
