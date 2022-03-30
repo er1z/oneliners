d05444ff0504d5c0c5bae34d238170160578465b
Przemysław eRIZ Pawliczuk <er1z@users.noreply.github.com>
2022-03-30 23:49:37 +0200

    Update lvm_expand_disk_in_vm.sh

--- lvm_expand_disk_in_vm.sh
+++ lvm_expand_disk_in_vm.sh
@@ -1,5 +1,6 @@
 # https://unix.stackexchange.com/a/98374/261284
-cfdisk /dev/sda1
+sudo parted /dev/sda
+parted> resizepart
 sudo reboot
 sudo pvresize /dev/sda1
 sudo lvextend -r -l +100%FREE /dev/VGNAME/LVNAME
